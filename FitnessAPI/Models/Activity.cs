07d6a5821b5b76598965867137ebc2641e3f07ef
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2024-09-10 18:35:18 +0300

    User model, Activity model, UserLogin DTO, UserRegister DTO added

--- FitnessAPI/Models/Activity.cs
+++ FitnessAPI/Models/Activity.cs
@@ -2,5 +2,12 @@
 {
     public class Activity
     {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; } // Foreign key reference
+        public string Type { get; set; }
+        public double Distance { get; set; }
+        public int Duration { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
     }
 }
